2d02993a23c5330f713be23bf3c43aff76b540ad
heshangwei <ydhs1234@163.com>
2024-11-30 16:48:14 +0800

    feat：合约部署配置改为 Mantle Sepolia

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,14 +1,10 @@
-export const deploySymbol = "AVAX";
-export const deployNetId = "0xa869";
-export const deployNetName = "Avalanche Fuji";
-export const deployNetRPC = "https://avalanche-fuji.infura.io/v3/e19e2e2cd089427f8e4eb8636cff1d5a";
+export const deploySymbol = "MNT";
+export const deployNetId = "0x138b";
+export const deployNetName = "Mantle Sepolia";
+export const deployNetRPC = "https://mantle-sepolia.g.alchemy.com/v2/VG6qN2lT5f99swpLvpyZQQDSn1bCuAD6";
 export const traceDomain = "https://testnet.snowtrace.io/address";
-// export const nftContractAddr = "0x83211296Df9A6Cff55A90604664d6B96e7c97a5C";
-// export const watContractAddr = "0x7B182A5Cc68124010c2110d92738707DBA4AFE90";
-// export const nftContractAddr = "0xD4ea678D1031562485F3B5E7806940A7FB536412";
-// export const watContractAddr = "0xDCFA876dE9B64Ca3Ab96e203dEcc2f8054FB410F";
-export const nftContractAddr = "0x4373C344C765A185e5CB24eD01e2f46621a8F204";
-export const watContractAddr = "0x2782aF1C3e5Ef69894a20564BD3357cAd58f6917";
+export const nftContractAddr = "0x7274376518e5318025Da518Bf8b2dbc5d172ce44";
+export const watContractAddr = "0x8C4fe3DEf87336a6a5D77CD976c181Ec73df51E7";
 // 菜单列表
 export const menus = [
   { key: "nft", label: "Plant NFT", path: "/nft-page", desc: `A kind of non-fungible token which is about the plant.` },
--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,9 +1,9 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { mainnet, avalancheFuji } from "wagmi/chains";
+import { avalancheFuji, mainnet, mantleSepoliaTestnet } from "wagmi/chains";
 
 export const wagmiConfig = getDefaultConfig({
   appName: "web3_learning",
   projectId: "b74ff78a8256eac550805546a75fe14d",
-  chains: [mainnet, avalancheFuji],
+  chains: [mainnet, avalancheFuji, mantleSepoliaTestnet],
   ssr: true,
 });
