b80d0c7281d0f21d933894af5eb725bc4c1032f2
heshangwei <ydhs1234@163.com>
2024-11-30 23:39:28 +0800

    feat：少了个空格

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
+import { deployNetName } from "@/utils/constants";
 import { Image } from "@nextui-org/react";
 import HomeCarousel from "./components/HomeCarousel";
-import { deployNetName } from "@/utils/constants";
 
 export default function HomePage() {
   return (
@@ -14,7 +14,7 @@ export default function HomePage() {
         />
         <div className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-center">Welcome to my Web3 ground</div>
         <div className="md:text-xl lg:text-2xl text-center">
-          I will make demos here when some funny ideas occurred to me. And all the contracts have been deployed on the
+          I will make demos here when some funny ideas occurred to me. And all the contracts have been deployed on the{" "}
           {deployNetName} network.
         </div>
       </div>
